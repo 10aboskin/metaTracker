30d5e5b212af649694d8ff1c3a30c03d2275d10a
Andrew <andrew@xapix.io>
2018-06-09 20:42:14 +0200

    update metaScore calculation

--- middlewares/getData.js
+++ middlewares/getData.js
@@ -9,18 +9,40 @@ const requestOptions = {
   transform: (body) => cheerio.load(body)
 }
 
-const _calculateMetaScore = (popularity, winrate) => {
-  return Math.floor(Math.sqrt(Math.pow(popularity, 2) + Math.pow(winrate, 2)))
+const _getVectorMagnitude = (x, y) => {
+  return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2)).toFixed(2)
+}
+
+const _calculateMetaScores = (popularites, winrates) => {
+  const normalizedPopularites = _normalize(popularites)
+  const normalizedWinrates = _normalize(winrates)
+  let metaScores = []
+  for (let i = 0; i < popularites.length; i++) {
+    metaScores[i] = _getVectorMagnitude(normalizedPopularites[i], normalizedWinrates[i])
+  }
+  return metaScores
+}
+
+const _normalize = (dataSet) => {
+  const max = Math.max(...dataSet)
+  const min = Math.min(...dataSet)
+  return dataSet.map(value => {
+    return (value - min) / (max - min)
+  })
 }
 
 const _createHeroObjects = (heroes, popularities, winrates) => {
+  const parsedPopularites = popularities.map(value => parseFloat(value))
+  const parsedWinrates = winrates.map(value => parseFloat(value))
+  const metaScores = _calculateMetaScores(parsedPopularites, parsedWinrates)
+
   let heroObjects = []
   for (let i = 0; i < heroes.length; i++) {
     const name = heroes[i]
     const role = heroRoles[name.toUpperCase()]
-    const popularity = parseFloat(popularities[i])
-    const winrate = parseFloat(winrates[i])
-    const metaScore = _calculateMetaScore(popularity, winrate)
+    const popularity = parsedPopularites[i]
+    const winrate = parsedWinrates[i]
+    const metaScore = metaScores[i]
     heroObjects[i] = { name, metaScore, role }
   }
   return heroObjects
@@ -29,10 +51,12 @@ const _createHeroObjects = (heroes, popularities, winrates) => {
 const getData = (req, res, next) => {
   rp(requestOptions)
     .then($ => {
+      // get data from table
       cheerioTableparser($)
       const [, heroes,,, popularities, winrates,] = $('table')
         .parsetable(true, true, true)
         .map(col => col.slice(2))
+      // attach hero objects to request
       req.heroObjects = _createHeroObjects(heroes, popularities, winrates)
       next()
     })
